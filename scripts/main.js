1618c552db0ab6a4b615dc84b8cccba31015a520
lvontay <lvontaythekid7@gmail.com>
2021-05-24 14:06:39 -0600

    Added js to the mix

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+const myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello, world!';
+alert('Hello')
+/* Variable */
+// This is a comment
+let myVariable = 'Lvontay';
+myvariable = 'Steve';
